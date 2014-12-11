b4269366cf8dae165634a7a5c757366c6d79f51a
Robert Ing <rmi22186@gmail.com>
2014-12-11 14:33:36 -0800

    branch behind

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,35 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
+  var nextFifo = 1;
+  var totaldequeue = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function(){
+    if (counter > 0) { 
+      totaldequeue++;
+      var item = storage[nextFifo];
+      nextFifo++;
+      return item;
+    }
+    
   };
 
   someInstance.size = function(){
+    var size = (counter - totaldequeue);
+    if (size<0) {
+      return 0
+    }
+    return size
   };
 
   return someInstance;
 };
+
+// 1: "a"
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    if (counter > 0) { 
+      var item = storage[counter]
+      counter--;
+      return item;
+    }
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
