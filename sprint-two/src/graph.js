50b25260cd206e8aecc008cc603f17ae9ba95386
Robert Ing <rmi22186@gmail.com>
2014-12-14 13:56:14 -0800

    about to merge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,20 @@
-
-
 var Graph = function(){
+	this.value = null;
+	this.storage = [];
 };
 
 Graph.prototype.addNode = function(node){
+	this.value = node;
+	this.storage.push(node);
 };
 
 Graph.prototype.contains = function(node){
+	return this.storage.indexOf(node) >= 0;
 };
 
 Graph.prototype.removeNode = function(node){
+	var removalIndex = this.storage.indexOf(node);
+	this.storage.splice(removalIndex,1);
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
@@ -27,6 +32,3 @@ Graph.prototype.forEachNode = function(cb){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
-
